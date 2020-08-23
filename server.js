b0f5254c90777a6e3d7614fe3e59b19e47929aec
gkampitakis <gkabitakis@gmail.com>
2020-08-23 09:58:32 +0100

    Setup basic server for testing

---
+++ server.js
@@ -0,0 +1,44 @@
+const fastify = require('fastify')({
+  logger: true
+});
+const MongoClient = require('mongodb').MongoClient;
+const customHealthCheck = require('fastify-custom-healthcheck');
+const _package = require('./package.json');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: `./.env.${process.env.NODE_ENV}`
+});
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT;
+
+
+fastify.register(customHealthCheck, {
+  path: '/api/health',
+  info: {
+    example: 'Custom Info',
+    env: process.env.NODE_ENV,
+    name: _package.name,
+    version: _package.version
+  },
+  exposeFailure: true
+})
+  .then(() => {
+    fastify.addHealthCheck('mongo', async () => {
+      const client = await MongoClient.connect(MONGODB_URL);
+      client.db('example');
+
+      client.close();
+    });
+  });
+
+fastify.get('/', (req, res) => {
+  res.status(200).send({
+    message: 'Nginx Nodejs Server'
+  });
+});
+
+fastify.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Example Server listening on port ${PORT}`);
+});
